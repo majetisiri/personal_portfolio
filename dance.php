7d653fd1254117ab339ef2058ab72184bc5cd51d
majetisiri <majetisiri@gmail.com>
2018-11-08 12:35:04 -0500

    media queries for dance.php and zoom for blog

--- dance.php
+++ dance.php
@@ -7,6 +7,15 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <!-- The above 4 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+    <!-- Global site tag (gtag.js) - Google Analytics -->
+    <script async src="https://www.googletagmanager.com/gtag/js?id=UA-128566543-1"></script>
+    <script>
+      window.dataLayer = window.dataLayer || [];
+      function gtag(){dataLayer.push(arguments);}
+      gtag('js', new Date());
+
+      gtag('config', 'UA-128566543-1');
+    </script>
 
     <!-- Title  -->
     <title>Srividya Majeti | Home</title>
@@ -20,6 +29,61 @@ include 'views.php';
 <style> 
     video::-webkit-media-controls-overlay-play-button {
       display: none;
+    }
+
+    * CSS used here will be applied after bootstrap.css */
+
+    .card {
+        -webkit-column-count: 1;
+        -moz-column-count: 1;
+        column-count: 1;
+    }
+
+  /*
+  Small devices (landscape phones, 34em and up ( 544px ))
+  */
+    @media (min-width: 34em) {
+        .card {
+            -webkit-column-count: 2;
+            -moz-column-count: 2;
+            column-count: 2;
+        }
+        
+    }
+
+  /*
+  Medium devices (tablets, 48em and up (768px))
+  */
+    @media (min-width: 48em) {
+        .card {
+            -webkit-column-count: 3;
+            -moz-column-count: 3;
+            column-count: 3;
+        }
+
+    }
+
+  /* 
+  Large devices (desktops, 62em and up(992px) )
+  */
+    @media (min-width: 62em) {
+        .card {
+            -webkit-column-count: 6;
+            -moz-column-count: 6;
+            column-count: 6;
+        }
+        
+    }
+     
+  /*
+  Extra large devices (large desktops, 75em and up (1200px))
+  */
+    @media (min-width: 75em ) {
+        .card {
+            -webkit-column-count: 7;
+            -moz-column-count: 7;
+            column-count: 7;
+        }
     }
 </style>
 <body>
@@ -124,6 +188,19 @@ include 'views.php';
                   </div>
                 </div>
             </div>
+            <!-- <div style="padding: 20px;">
+                <div class="card" style="width: 28rem;padding-left: 20px;padding-right: 20px;">
+                  <video id="3" class="videoPlayer" onclick="getVideoId(this)"  width="400" controls controlsList="nodownload">
+                    <source src="img/dance/shape-of-you.mp4" type="video/mp4">
+                  </video>
+                  <div class="card-body">
+                    <h5 class="card-title">
+                        Shape of you- Classical
+                        <p></p>
+                    </h5>
+                  </div>
+                </div>
+            </div> -->
         </div>
     </section>
     <!-- ***** Welcome Area End ***** -->
